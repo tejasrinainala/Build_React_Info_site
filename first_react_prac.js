d94611f58d99fcccd85c5bdefb4ac0d952a603cd
tejasrinainala <143178876+tejasrinainala@users.noreply.github.com>
2024-06-21 12:03:20 +0530

    Create first_react_prac.js

---
+++ first_react_prac.js
@@ -0,0 +1,16 @@
+/* Surprise! You probably thought you could just forget
+the line of code you just learned! Nope, not on my watch!
+
+Try to write that 1-liner of React code again! This time,
+see if you can figure out how to render an <ul> with 2+ <li>s inside*/
+
+
+
+
+//solution
+ReactDOM.render(<ul><li>How do you do?</li><li>How about learning react?!</li></ul>,document.getElementById("root"))
+
+
+//output when attached to a html file.
+. How do you do?
+. How about learning react?!
